ed0f26d2062e87cc53e32559a974d19ba5659112
Kether <Kether0228@gmail.com>
2025-03-30 23:20:52 +0800

    帳號功能-新增單元測試

--- src/test/java/com/kether/springbootmall/controller/UserControllerTest.java
+++ src/test/java/com/kether/springbootmall/controller/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
